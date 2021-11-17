5dfa759868d60835e3c2ecb95e9781b8fbff92b9
PC-VITO\user <yohanes.vito@ti.ukdw.ac.id>
2021-11-17 11:17:13 +0700

    tambah soal 8

---
+++ soal8 - bermain pola.py
@@ -0,0 +1,14 @@
+#Buatlah program untuk membuat pola berikut ini
+
+
+
+n = str(input("Mendatar/Menurun?: "))
+
+if(n== "Menurun"):
+    m = int(input("Masukkan baris: "))
+    print('*\n'*m)
+elif(n== "Mendatar"):
+    m = int(input("Masukkan kolom: "))
+    print('#'*m)
+else:
+    print("Pola tidak dikenali ")
